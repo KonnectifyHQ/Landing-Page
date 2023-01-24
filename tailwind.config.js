0f1434f2355777cb4ec07fb6117791f90525fb32
mtdola <mtdolaj@gmail.com>
2023-01-24 15:25:21 +0100

    Design done. Testing for mobile now

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dms: ["DM Sans", "sans-serif"]
+      },
+      colors : {
+        primary : '#04FF1D',
+        'primary-l' : '#E1FEE4' 
+      }
+    },
+  },
+  plugins: [],
+}
